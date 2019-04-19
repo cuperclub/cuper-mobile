3f79ff65cceaa9baf4cef74e22deb9be7ae0067b
Lenin Capa <leviskp87@gmail.com>
2019-04-19 09:21:22 -0500

    fix translate unit test

--- src/app/shared/onboarding/onboarding.page.spec.ts
+++ src/app/shared/onboarding/onboarding.page.spec.ts
@@ -2,6 +2,8 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OnboardingPage } from './onboarding.page';
+import { RouterTestingModule } from '@angular/router/testing';
+import {TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('OnboardingPage', () => {
   let component: OnboardingPage;
@@ -9,8 +11,20 @@ describe('OnboardingPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        })
+      ],
       declarations: [ OnboardingPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        TranslateService
+      ]
     })
     .compileComponents();
   }));
